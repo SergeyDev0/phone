e99d91ce510c20e737abe6488d04155fade1a565
makarkapar <makarkar@gmail.com>
2022-08-24 08:54:39 +0300

    commit

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const time = new Date();
 let hours = time.getHours();
 let minutes = time.getMinutes();
+
+if( hours < 10 ){
+    hours = '0' + hours;
+}
+
+if( minutes < 10 ){
+    minutes += '0' + minutes;
+}
+
 let fullTime = `${hours}:${minutes}`;
 
 document.querySelector('.system-curtain .time').textContent = fullTime;
